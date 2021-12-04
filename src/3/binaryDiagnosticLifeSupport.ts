899c022f2b7465bd0090e3eb4d9693a6715dca1e
Florin Mirosnicencu <florinmirosnicencu@gmail.com>
2021-12-04 18:05:04 +0000

    D3P2 Refactor solution to remove a lot of duplicated code

--- src/3/binaryDiagnosticLifeSupport.ts
+++ src/3/binaryDiagnosticLifeSupport.ts
@@ -1,71 +1,55 @@
-import {BinaryValueCounter, countDigit, parseInput} from "./binaryDigitHelper";
+import {BinaryValueCounter, parseInput} from "./binaryDigitHelper";
 
 interface LifeSupportReport {
     o2: string,
     co2: string
 }
 
-function calculateO2(digitPosition: number, maxDigitCount: number, digitAppearances: BinaryValueCounter[], RowList: string[]): string {
-    let binaryString = "";
-    let filteredRows = RowList;
-    while (digitPosition < maxDigitCount) {
-        digitAppearances = [];
-        filteredRows.map((row, index) => {
-            const digitArray = row.split("");
-            const digit = parseInt(digitArray[digitPosition]);
-
-            digitAppearances = countDigit(digitAppearances, digit, digitPosition);
-        });
-
-        digitAppearances.map((value) => {
-            if (value["1"] >= value["0"]) {
-                binaryString += "1";
-                return;
-            }
-
-            binaryString += "0";
-            return;
-        })
-        const mostCommonValueForCurrentPosition = digitAppearances[digitPosition][1] >= digitAppearances[digitPosition][0] ? "1" : "0";
-        filteredRows = filteredRows.filter(element => {
-            return element[digitPosition] === mostCommonValueForCurrentPosition;
-        })
-
-        if (filteredRows.length === 1) {
-            return filteredRows[0];
-        }
-
-        digitPosition++;
-    }
-
-    return binaryString;
+function resetDigitAppearances(): BinaryValueCounter {
+    return {
+        0: 0,
+        1: 0
+    };
 }
 
-function calculateCo2(digitPosition: number, maxDigitCount: number, digitAppearances: BinaryValueCounter[], RowList: string[]): string {
+function calculateDigitCount(filteredRows: string[], digitPosition: number) {
+    let digitAppearances = resetDigitAppearances();
+    filteredRows.map((row) => {
+        const digitArray = row.split("");
+        const digit = parseInt(digitArray[digitPosition]);
+
+        // @ts-ignore
+        digitAppearances[digit]++;
+    });
+    return digitAppearances;
+}
+
+function processDigitCountForCo2(digitAppearances: BinaryValueCounter) {
+    if (digitAppearances["1"] >= digitAppearances["0"]) {
+        return "0";
+    }
+    return "1";
+}
+
+function processDigitCountForO2(digitAppearances: BinaryValueCounter) {
+    if (digitAppearances["1"] >= digitAppearances["0"]) {
+        return "1";
+    }
+    return "0";
+}
+
+function processRows(RowList: string[], digitCountProcessor: (digitAppearances: BinaryValueCounter) => string): string {
     let binaryString = "";
     let filteredRows = RowList;
+    const maxDigitCount = RowList[0].split('').length;
+    let digitPosition = 0;
+
     while (digitPosition < maxDigitCount) {
-        digitAppearances = [];
-        filteredRows.map((row, index) => {
-            const digitArray = row.split("");
-            const digit = parseInt(digitArray[digitPosition]);
-
-            digitAppearances = countDigit(digitAppearances, digit, digitPosition);
-        });
-
-        digitAppearances.map((value) => {
-            if (value["0"] <= value["1"]) {
-                binaryString += "0";
-                return;
-            }
-
-            binaryString += "1";
-            return;
-        })
-
-        const mostCommonValueForCurrentPosition = digitAppearances[digitPosition][0] <= digitAppearances[digitPosition][1] ? "0" : "1";
+        const digitAppearances = calculateDigitCount(filteredRows, digitPosition);
+        const calculatedDigit = digitCountProcessor(digitAppearances);
+        binaryString += calculatedDigit;
         filteredRows = filteredRows.filter(element => {
-            return element[digitPosition] === mostCommonValueForCurrentPosition;
+            return element[digitPosition] === calculatedDigit;
         })
 
         if (filteredRows.length === 1) {
@@ -83,14 +67,10 @@ export function binaryDiagnosticLifeSupport(input: string): LifeSupportReport {
         o2: "",
         co2: ""
     }
-
-    let digitAppearances: BinaryValueCounter[] = [];
-
     const RowList = parseInput(input);
-    const maxDigitCount = RowList[0].split('').length;
-    let digitPosition = 0;
-    result.o2 = calculateO2(digitPosition, maxDigitCount, digitAppearances, RowList);
-    result.co2 = calculateCo2(digitPosition, maxDigitCount, digitAppearances, RowList);
+
+    result.o2 = processRows(RowList, processDigitCountForO2);
+    result.co2 = processRows(RowList, processDigitCountForCo2);
 
     return result;
 }
