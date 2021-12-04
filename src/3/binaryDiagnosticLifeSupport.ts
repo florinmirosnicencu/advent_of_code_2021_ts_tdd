f8e407850876274bf3cbd1dae59e5399b2a231b3
Florin Mirosnicencu <florinmirosnicencu@gmail.com>
2021-12-04 09:45:39 +0000

    D3P2 Hack in 2 digit solution, need to refactor

--- src/3/binaryDiagnosticLifeSupport.ts
+++ src/3/binaryDiagnosticLifeSupport.ts
@@ -14,13 +14,17 @@ export function binaryDiagnosticLifeSupport(input: string): LifeSupportReport {
     let digitAppearances: BinaryValueCounter[] = [];
 
     const RowList = parseInput(input);
+    let maxDigitCount = 0;
+    let digitPosition = 0;
     RowList.map((row, index) => {
-        const digit = parseInt(row);
+        const digitArray = row.split("");
+        maxDigitCount = digitArray.length;
+        const digit = parseInt(digitArray[digitPosition]);
         if (isNaN(digit)) {
             return;
         }
 
-        digitAppearances = countDigit(digitAppearances, digit, 1);
+        digitAppearances = countDigit(digitAppearances, digit, digitPosition);
     });
 
     digitAppearances.map((value) => {
@@ -38,6 +42,35 @@ export function binaryDiagnosticLifeSupport(input: string): LifeSupportReport {
         result.co2 += "1"
         return
     })
+    digitAppearances = [];
+    if (maxDigitCount > 1) {
+        digitPosition = 1;
+        RowList.map((row, index) => {
+            const digitArray = row.split("");
+            const digit = parseInt(digitArray[digitPosition]);
+            if (isNaN(digit)) {
+                return;
+            }
+
+            digitAppearances = countDigit(digitAppearances, digit, digitPosition);
+        });
+
+        digitAppearances.map((value) => {
+            if (value["1"] == undefined && value["0"] == undefined) {
+                return;
+            }
+
+            if (value["1"] >= value["0"]) {
+                result.o2 += "1";
+                result.co2 += "0"
+                return
+            }
+
+            result.o2 += "0";
+            result.co2 += "1"
+            return
+        })
+    }
 
     return result;
 }
